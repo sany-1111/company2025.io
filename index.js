a479a354b5acd77ce5676a5d79c4fed1e2265c93
sany-1111 <huangyushan602@gmail.com>
2025-05-30 17:25:54 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function login() {
+  const user = document.getElementById("username").value.trim();
+  const pass = document.getElementById("password").value.trim();
+  const error = document.getElementById("loginError");
+
+  fetch(`https://script.google.com/macros/s/AKfycbwzx3Xw3AG5nlirhS89yVM9gb-6vMAI8KyI9BznZDaWKEvi71epGMvDD7YQDgu4I_bx/exec?username=${encodeURIComponent(user)}&password=${encodeURIComponent(pass)}`)
+    .then(res => res.text())
+    .then(role => {
+      if (role === "staff") {
+        localStorage.setItem("username", user);
+        localStorage.setItem("role", role);
+        window.location.href = "leave.html";
+      } else if (role === "manager" || role === "admin") {
+        localStorage.setItem("username", user);
+        localStorage.setItem("role", role);
+        window.location.href = "manager.html";
+      } else {
+        error.textContent = "❌ 帳號或密碼錯誤";
+      }
+    })
+    .catch(err => {
+      error.textContent = "❌ 登入失敗：" + err;
+    });
